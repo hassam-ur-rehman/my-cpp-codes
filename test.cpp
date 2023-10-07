537df5720628c9659a751da2e0e01fd7caac9f76
hassam <hassamurrehman2002@gmail.com>
2023-10-07 23:43:14 +0500

    checking git and github

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"hello world"<<endl;
+    cout<< "i am krumchus"<<endl;
+    int a = 4;
+    cout<<a<<endl;
+    cout<<"hello"<<endl;
+}
